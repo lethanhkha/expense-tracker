584d7c0e405653152c3b6d4303998817bc5c7112
snow6805 <123850471+snow6805@users.noreply.github.com>
2025-08-23 16:39:55 +0700

    create auth api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -19,6 +20,11 @@ app.use(
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+// Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
